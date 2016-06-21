30caa92fb0886902a6c6c101715df47e281c6fc5
Chintan Patel <chintan.patel@briqsdata.com>
2016-06-21 13:05:10 +0530

    Fix lambda runner

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
 exports.handler = function (event, context) {
-	console.log(event);
+	fs.writeFile('/var/log/worker.log', 'Just now, we have created this file', function (err) {
+        if (err) throw err;
+        console.log('It\'s saved! in same location.');
+    });
 }
